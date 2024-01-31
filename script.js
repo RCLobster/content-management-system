a983c328c986b3319d1e2abb0d5388e9d98c6c99
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-01-30 17:42:09 -0800

    able to view all departments and roles, also able to add a new department

--- script.js
+++ script.js
@@ -27,34 +27,80 @@ const db = mysql.createConnection(
 // MAIN CODE
 
 const mainMenu = () => {
+    // add a line spacer before each time main menu asks the user a question
+    console.log("\n");
+
+    // prompt the user to select an option from the list
     inquirer.prompt([{
             type: "list",
             message: "What would you like to do?",
             name: "main_menu",
             choices: [
                 "View all employees",
+                "View all departments",
+                "View all roles",
                 "Add a department",
                 "Add a role",
                 "Add an employee",
                 "Update an employee role"
             ]
         }]).then(answers => {
+            // depending on user's choice, run the corresponding function
             if(answers.main_menu === "View all employees"){
-                console.log("view employees");
                 viewAllEmployees();
+
+            } else if(answers.main_menu === "Add a department"){
+                addDepartment();
+            } else if(answers.main_menu === "View all departments"){
+                viewAllDepartments();
+            } else if(answers.main_menu === "View all roles"){
+                viewAllRoles();
             }
         })
 
-}
+};
 
 const viewAllEmployees = () => {
-    console.log("Here are all the employees");
-    db.query('SELECT * FROM employee', function (err, results) {
-      console.table(results);
+    console.log("\nHere are all the employees");
+    db.query('SELECT * FROM employee', function (err, data) {
+      console.table(data);
+
+      mainMenu();
     });
-}
+};
 
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the new department?",
+            name: "dept_name"
+        }
+    ]).then(answers => {
+        db.query(`INSERT INTO department (name) VALUES ("${answers.dept_name}")`, (err, data) => {
+            console.log(`New department created: ${answers.dept_name}`);
+            mainMenu();
+        });
+    });
+};
 
+const viewAllDepartments = () => {
+    console.log("\nHere are all the departments");
+    db.query('SELECT * FROM department', function (err, data) {
+      console.table(data);
+
+      mainMenu();
+    });
+};
+
+const viewAllRoles = () => {
+    console.log("\nHere are all the roles");
+    db.query('SELECT * FROM role', function (err, data) {
+      console.table(data);
+
+      mainMenu();
+    });
+};
 
 
 
