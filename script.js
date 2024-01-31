54be9e9a9354642cd68d72926b75759b36616408
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-01-30 16:43:11 -0800

    basic schema created

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // import code depdendencies 
 const express = require('express');
 const mysql = require('mysql2');
+const inquirer = require('inquirer');
 
 // set up port to host server
 const PORT = process.env.PORT || 3001;
@@ -23,10 +24,17 @@ const db = mysql.createConnection(
   console.log(`Connected to the classlist_db database.`)
 );
 
+// MAIN CODE
 
 db.query('SELECT * FROM students', function (err, results) {
   console.log(results);
 });
+
+
+
+
+
+
 
 // if we get a 404 error end the server
 app.use((req, res) => {
