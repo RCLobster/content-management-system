319742d5c75163a2cd4716a7f534b5c82fd10ce1
RCLobster <44736025+RCLobster@users.noreply.github.com>
2024-01-30 16:31:56 -0800

    server runs on local port

---
+++ script.js
@@ -0,0 +1,39 @@
+// import code depdendencies 
+const express = require('express');
+const mysql = require('mysql2');
+
+// set up port to host server
+const PORT = process.env.PORT || 3001;
+
+// initilize the app
+const app = express();
+
+//set up middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// boot up and connect to the mySQL database by sending relevant data
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'mysqlpassword',
+    database: 'company_db'
+  },
+  console.log(`Connected to the classlist_db database.`)
+);
+
+
+db.query('SELECT * FROM students', function (err, results) {
+  console.log(results);
+});
+
+// if we get a 404 error end the server
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+// listen for an interaction on the designated PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
